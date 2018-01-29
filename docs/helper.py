b4dbfa196d3da21d932f7a4453589b86d8998148
TopKeingt <ppkhiemnguyen@gmail.com>
2018-01-29 15:19:28 -0500

    Helper for Analysis

--- docs/helper.py
+++ docs/helper.py
@@ -1,54 +1,61 @@
 def help_(DecOrEn,OutOrIn):
     about_me = '''
-        Hi!!, My name is {0}Khiem Nguyen{1} aka {2}TopKeingt{1}. 
-        
+        Hi!!, My name is {0}Khiem Nguyen{1} aka {2}TopKeingt{1}.
+
         I'm a student in Malden High School and love to code.I trying to learn as many programming
         languages as possible.
-        
-        If you found some bug, please let me know because not everything is perfect by pulling request at 
-        my github link {3}{4}https://github.com/TopKeingt/EnciphDeWord{1} or straight to me 
-        {3}{4}ppkhiemnguyen{5}@{1}{3}{4}gmail.com{1}    
+
+        If you found some bug, please let me know because not everything is perfect by pulling request at
+        my github link {3}{4}https://github.com/TopKeingt/EnciphDeWord{1} or straight to me
+        {3}{4}ppkhiemnguyen{5}@{1}{3}{4}gmail.com{1}
     '''
     options =  '''
       {0}1{1}.{2} Encrypt{1}
-      {0}2{1}.{2} Decrypt{1} 
-      {0}3{1}.{2} Help 
-      {0}4{1}.{2} About me{1}
-      {0}5{1}.{2} Banner {1} 
-      {0}6{1}.{2} Exit{1} 
+      {0}2{1}.{2} Decrypt{1}
+      {0}3{1}.{2} Analysis{1}
+      {0}4{1}.{2} Help
+      {0}5{1}.{2} About me{1}
+      {0}6{1}.{2} Banner {1}
+      {0}7{1}.{2} Exit{1}
       '''
+    help_analysis = '''
+    {0}help{1}            -- Display this help message
+    {0}show info{1}       -- Display information box
+    {0}SET[Cipher Text]{1}-- Set encrypted string
+    {0}back{1}            -- Back to main
+    '''
     help_outside_encrypt = '''
-    {0}help{1}            -- Display this help message 
+    {0}help{1}            -- Display this help message
     {0}show options{1}    -- Show encryption types
     {0}use [numbers]{1}   -- Use encryption types
-    {0}back{1}            -- back to main 
+    {0}back{1}            -- Back to main
     '''
-    
+
     help_inside_encrypt = '''
     {0}help{1}                                -- Display this help message
     {0}profile{1}                             -- Display profile of this encryption
     {0}show info{1}                           -- Show info box of your encrypter
-    {0}SET (message/key) [string/number]{1}   -- set message or key to your given string or number
+    {0}SET (message/key) [string/number]{1}   -- Set message or key to your given string or number
     {0}message{1}                             -- A wizard type if you don't want to use SET command
     {0}key{1}                                 -- A wizard type if you don't want to use SET command
     {0}execute{1}                             -- Run the encryption
-    {0}back{1}                                -- back to encrypt choices 
+    {0}back{1}                                -- Back to encrypt choices
     '''
     help_inside_decrypt = '''
     {0}help{1}                                -- Display this help message
     {0}profile{1}                             -- Display profile of this encryption
     {0}show info{1}                           -- Show info box of your decrypter
-    {0}SET (message/key) [string/number]{1}   -- set encrypted message or key to your given string or number
+    {0}SET (message/key) [string/number]{1}   -- Set encrypted message or key to your given string or number
     {0}message{1}                             -- A wizard type if you don't want to use SET command
     {0}key{1}                                 -- A wizard type if you don't want to use SET command
     {0}execute{1}                             -- Run the decryption
-    {0}back{1}                                -- back to encrypt choices 
+    {0}back{1}                                -- back to decrypt choices
     '''
     help_outside_decrypt = '''
-    {0}help{1}             -- Display this help message 
+    {0}help{1}             -- Display this help message
     {0}show options{1}     -- Show decryption types
     {0}use [numbers]{1}    -- Use decryption types
-    {0}back{1}             -- back to main 
+    {0}back{1}             -- Back to main
     '''
     msg_helper = "Message that gonna {}"
     key_helper = "Key for {}"
@@ -74,9 +81,5 @@ def help_(DecOrEn,OutOrIn):
             return result_helper
     elif DecOrEn == 'author':
         return about_me
-    
-    
-    
-    
-    
-        
+    elif DecOrEn == 'analysis':
+        return help_analysis
